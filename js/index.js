3399e43e4cd705a6c39f4317d1e0301136948ef3
AndresRamirez917 <andresmintic2021@gmail.com>
2024-09-25 20:10:59 -0500

    first commit

--- js/index.js
+++ js/index.js
@@ -13,4 +13,42 @@ function addNav(){
     }
 }
 
+async function getData(){
+const result = await fetch('https://rickandmortyapi.com/api/character');
+const character = await result.json();
+const jsonArr = character.results.map(elemento => Object.entries(elemento));
+console.log(jsonArr)
+const jsonSlice = jsonArr.slice(0, 4);
+character.results.forEach(element => {
+    const randInt = randomData(1, jsonArr.length);
+    const randIndex = randInt;
+    for(i = 0; i < jsonSlice.length; i++){
+        if(element.id == i){
+            const card = document.createRange().createContextualFragment(`
+                
+            <div class="card">
+                <img src="${jsonArr[randIndex][8][1]}">
+                <h2>${jsonArr[randIndex][1][1]}</h2>
+                <h3>Lorem ipsum dolor sit amet, consectetur adipiscing elit</h3>
+                <div class="list">
+                    <ul>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</li>
+                        <li>Lorem ipsum dolor sit amet, consectetur adipiscing elit ut aliquam, purus sit amet luctus venenatis</li>
+                    </ul>
+                </div>
+            </div>
+                
+                `)
+                const features_row = document.querySelector('.features-row');
+                features_row.append(card)
+        }
+    }
+    function randomData(min, max){
+        return Math.floor(Math.random() * (min - max + 1) + max)
+    }
+});
+}
+getData()
+
 addNav()
